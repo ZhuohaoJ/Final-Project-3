66516b68648ea9a23c70ebc2b63c5d42469fcb27
Jay <zxz220008@gmail.com>
2023-05-01 17:55:44 -0500

    Changed script to allow addition of weapons

---
+++ Assets/ChangeWeapon.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeWeapon : MonoBehaviour
+{
+    [SerializeField] private GameObject _weapon1;
+    [SerializeField] private GameObject _weapon2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _weapon1.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            _weapon1.SetActive(true);
+            _weapon2.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            _weapon2.SetActive(true);
+            _weapon1.SetActive(false);
+        }
+    }
+}
